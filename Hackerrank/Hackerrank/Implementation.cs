bfe0b49bf1fab9ca9d9b9adb82a7129325c9d677
nikolaytsvetin <nikolaytsvetin@gmail.com>
2019-08-21 22:40:07 +0300

    Implementation section started.

---
+++ Hackerrank/Hackerrank/Implementation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank
+{
+    public static class Implementation
+    {
+        public static List<int> GradingStudents(List<int> grades)
+        {
+            List<int> modifiedGrades = new List<int>();
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                int currentGrade = grades[i];
+
+                if (currentGrade < 38)
+                {
+                    modifiedGrades.Add(currentGrade);
+                }
+                else
+                {
+                    int currentValueFromDivision = currentGrade / 5;
+                    int nextValue = currentValueFromDivision + 1;
+                    int nextNumber = nextValue * 5;
+
+                    if (nextNumber - currentGrade < 3)
+                    {
+                        currentGrade = nextNumber;
+                    }
+
+                    modifiedGrades.Add(currentGrade);
+                }
+            }
+
+            return modifiedGrades;
+        }
+
+        public static void CountApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            int houseStart = s;
+            int houseEnd = t;
+            int appleTreeLocation = a;
+            int orangeTreeLocation = b;
+            int applesCounter = 0;
+            int orangesCounter = 0;
+
+            for (int i = 0; i < apples.Length; i++)
+            {
+                if (apples[i] < 0)
+                {
+                    continue;
+                }
+
+                if (appleTreeLocation + apples[i] >= houseStart && appleTreeLocation + apples[i] <= houseEnd)
+                {
+                    applesCounter++;
+                }
+            }
+
+            for (int j = 0; j < oranges.Length; j++)
+            {
+                if (oranges[j] > 0)
+                {
+                    continue;
+                }
+
+                if (orangeTreeLocation + oranges[j] <= houseEnd && orangeTreeLocation + oranges[j] >= houseStart)
+                {
+                    orangesCounter++;
+                }
+            }
+
+            Console.WriteLine(applesCounter);
+            Console.WriteLine(orangesCounter);
+        }
+
+        public static string Kangaroo(int x1, int v1, int x2, int v2)
+        {
+            int positionOfTheFirstKangaroo = x1;
+            int positionOfTheSecondKangaroo = x2;
+            int speedOfTheFirstKangaroo = v1;
+            int speedOfTheSecondKangaroo = v2;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                if (positionOfTheFirstKangaroo == positionOfTheSecondKangaroo)
+                {
+                    return "YES";
+                }
+
+                positionOfTheFirstKangaroo += speedOfTheFirstKangaroo;
+                positionOfTheSecondKangaroo += speedOfTheSecondKangaroo;
+            }
+
+            return "NO";
+        }
+    }
+}
--- Hackerrank/Hackerrank/Program.cs
+++ Hackerrank/Hackerrank/Program.cs
@@ -11,9 +11,12 @@ namespace Hackerrank
         static void Main(string[] args)
         {
             List<List<int>> data = new List<List<int>>() { new List<int>() { 11, 2, 4 }, new List<int>() { 4, 5, 6 }, new List<int>() { 10, 8, -12 } };
-            int[] arr = new int[] { 5, 5, 5, 5, 5 };
+            int[] arr = new int[] { -2, 2, 1 };
+            int[] arr2 = new int[] { 5, -6 };
 
-            Console.WriteLine(Warmup.TimeConversion("07:05:45PM"));
+            List<int> numbers = new List<int>() { 73, 67, 38, 33 };
+
+            Implementation.CountApplesAndOranges(7, 11, 5, 15, arr, arr2);
         }
     }
 }
